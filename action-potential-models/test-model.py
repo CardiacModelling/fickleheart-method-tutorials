878f83c174f848f1aeb332af01498fab4ed59cdf
Chon Lok Lei <chonloklei@gmail.com>
2019-06-20 16:07:33 +0100

    Use Fink 2008 IKr model in tnnp-2004-w

--- action-potential-models/test-model.py
+++ action-potential-models/test-model.py
@@ -20,15 +20,15 @@ model_tnnp = m.Model('./mmt-model-files/tnnp-2004.mmt')
 model_tnnp.set_name('tnnp-2004')
 model_tnnpw = m.Model('./mmt-model-files/tnnp-2004-w.mmt')
 model_tnnpw.set_name('tnnp-w-2004')
-model_ohara = m.Model('./mmt-model-files/ohara-2011.mmt')
-model_ohara.set_name('ohara-2011')
+model_fink = m.Model('./mmt-model-files/fink-2008.mmt')
+model_fink.set_name('fink-2008')
 
 # Default stimuli
 times = np.linspace(0, 1000, 5000)
 
 sim_tnnp = model_tnnp.simulate(np.ones(model_tnnp.n_parameters()), times)
 sim_tnnpw = model_tnnpw.simulate(np.ones(model_tnnpw.n_parameters()), times)
-sim_ohara = model_ohara.simulate(np.ones(model_ohara.n_parameters()), times)
+sim_fink = model_fink.simulate(np.ones(model_fink.n_parameters()), times)
 
 for _ in range(10):
     assert(np.all(np.abs(
@@ -39,15 +39,15 @@ for _ in range(10):
                     times)
                 - sim_tnnpw) < 1e-6))
     assert(np.all(np.abs(
-                model_ohara.simulate(np.ones(model_ohara.n_parameters()),
+                model_fink.simulate(np.ones(model_fink.n_parameters()),
                     times)
-                - sim_ohara) < 1e-6))
+                - sim_fink) < 1e-6))
 
 # and have a look at it
 plt.figure()
 plt.plot(times, sim_tnnp, label=model_tnnp.name())
 plt.plot(times, sim_tnnpw, label=model_tnnpw.name())
-plt.plot(times, sim_ohara, label=model_ohara.name())
+plt.plot(times, sim_fink, label=model_fink.name())
 plt.legend()
 plt.xlabel('Time (ms)')
 plt.ylabel('Voltage (mV)')
@@ -57,7 +57,7 @@ else:
     plt.show()
 plt.close()
 
-del(model_tnnp, model_tnnpw, model_ohara)
+del(model_tnnp, model_tnnpw, model_fink)
 
 
 # Input stimuli
@@ -77,13 +77,13 @@ model_tnnp.set_name('tnnp-2004')
 model_tnnpw = m.Model('./mmt-model-files/tnnp-2004-w.mmt',
         stim_seq=random_stim)
 model_tnnpw.set_name('tnnp-w-2004')
-model_ohara = m.Model('./mmt-model-files/ohara-2011.mmt',
+model_fink = m.Model('./mmt-model-files/fink-2008.mmt',
         stim_seq=random_stim)
-model_ohara.set_name('ohara-2011')
+model_fink.set_name('fink-2008')
 
 sim_tnnp = model_tnnp.simulate(np.ones(model_tnnp.n_parameters()), times)
 sim_tnnpw = model_tnnpw.simulate(np.ones(model_tnnpw.n_parameters()), times)
-sim_ohara = model_ohara.simulate(np.ones(model_ohara.n_parameters()), times)
+sim_fink = model_fink.simulate(np.ones(model_fink.n_parameters()), times)
 
 for _ in range(10):
     assert(np.all(np.abs(
@@ -94,15 +94,15 @@ for _ in range(10):
                     times)
                 - sim_tnnpw) < 1e-6))
     assert(np.all(np.abs(
-                model_ohara.simulate(np.ones(model_ohara.n_parameters()),
+                model_fink.simulate(np.ones(model_fink.n_parameters()),
                     times)
-                - sim_ohara) < 1e-6))
+                - sim_fink) < 1e-6))
 
 # and have a look at it
 plt.figure()
 plt.plot(times, sim_tnnp, label=model_tnnp.name())
 plt.plot(times, sim_tnnpw, label=model_tnnpw.name())
-plt.plot(times, sim_ohara, label=model_ohara.name())
+plt.plot(times, sim_fink, label=model_fink.name())
 plt.legend()
 plt.xlabel('Time (ms)')
 plt.ylabel('Voltage (mV)')
