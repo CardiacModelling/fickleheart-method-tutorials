8f8c6f34386d7fcaa8ea80e0d3c3a132d441ec26
Chon Lok Lei <chonloklei@gmail.com>
2019-09-12 22:21:56 +0100

    Add tnnp-2004 posterior predictives

--- action-potential-models/posterior-all.sh
+++ action-potential-models/posterior-all.sh
@@ -16,3 +16,13 @@ python posterior.py fink-2008 stim1hz hergblock
 #python posterior.py fink-2008 randstim randstim
 #python posterior.py fink-2008 randstim hergblock
 
+# tnnp-2004 posterior predictions (check identifiability)
+python posterior.py tnnp-2004 stim1hz stim1hz
+python posterior.py tnnp-2004 stim1hz stim2hz
+python posterior.py tnnp-2004 stim1hz randstim
+python posterior.py tnnp-2004 stim1hz hergblock
+#python posterior.py tnnp-2004 randstim stim1hz
+#python posterior.py tnnp-2004 randstim stim2hz
+#python posterior.py tnnp-2004 randstim randstim
+#python posterior.py tnnp-2004 randstim hergblock
+
--- action-potential-models/posterior.py
+++ action-potential-models/posterior.py
@@ -18,7 +18,7 @@ import protocol
 Run posterior prediction.
 """
 
-model_list = ['tnnp-2004-w', 'fink-2008']
+model_list = ['tnnp-2004-w', 'fink-2008', 'tnnp-2004']
 cal_list = ['stim1hz', 'stim2hz', 'randstim']
 predict_list = ['stim1hz', 'stim2hz', 'randstim', 'hergblock']
 
