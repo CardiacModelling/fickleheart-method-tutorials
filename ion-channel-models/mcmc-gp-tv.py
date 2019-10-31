6a18b4716f2adf8f260d9b48e4fe70cb39dbda1a
chonlei <chonloklei@gmail.com>
2019-10-31 13:30:07 +0000

    Add GP discrepancy model as function of time and voltage

---
+++ ion-channel-models/mcmc-gp-tv.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+from __future__ import print_function
+import sys
+sys.path.append('./method')
+import os
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import pints
+import pints.io
+import pints.plot
+from scipy.interpolate import interp1d
+
+import model as m
+import parametertransform
+import priors
+from priors import HalfNormalLogPrior, InverseGammaLogPrior
+from sparse_gp_custom_likelihood import DiscrepancyLogLikelihood
+
+"""
+Run MCMC.
+"""
+
+model_list = ['A', 'B', 'C']
+
+try:
+    which_model = sys.argv[1] 
+except:
+    print('Usage: python %s [str:which_model]' % os.path.basename(__file__))
+    sys.exit()
+
+if which_model not in model_list:
+    raise ValueError('Input model %s is not available in the model list' \
+            % which_model)
+
+# Get all input variables
+import importlib
+sys.path.append('./mmt-model-files')
+info_id = 'model_%s' % which_model
+info = importlib.import_module(info_id)
+
+data_dir = './data'
+
+savedir = './out/mcmc-' + info_id + '-gp-tv'
+if not os.path.isdir(savedir):
+    os.makedirs(savedir)
+
+data_file_name = 'data-sinewave.csv'
+print('Fitting to ', data_file_name)
+print('Temperature: ', info.temperature)
+saveas = info_id + '-' + data_file_name[5:][:-4]
+
+# Protocol
+protocol = np.loadtxt('./protocol-time-series/sinewave.csv', skiprows=1,
+        delimiter=',')
+protocol_times = protocol[:, 0]
+protocol = protocol[:, 1]
+
+
+# Control fitting seed
+# fit_seed = np.random.randint(0, 2**30)
+fit_seed = 542811797
+print('Fit seed: ', fit_seed)
+
+np.random.seed(fit_seed)
+
+# Set parameter transformation
+transform_to_model_param = parametertransform.log_transform_to_model_param
+transform_from_model_param = parametertransform.log_transform_from_model_param
+
+# Load data
+data = np.loadtxt(data_dir + '/' + data_file_name,
+                  delimiter=',', skiprows=1)  # headers
+times = data[:, 0]
+data = data[:, 1]
+voltage = interp1d(protocol_times, protocol, kind='linear')(times)
+noise_sigma = np.std(data[:500])
+print('Estimated noise level: ', noise_sigma)
+
+model = m.Model(info.model_file,
+        variables=info.parameters,
+        current_readout=info.current_list,
+        set_ion=info.ions_conc,
+        transform=transform_to_model_param,
+        temperature=273.15 + info.temperature,  # K
+        )
+
+LogPrior = {
+        'model_A': priors.ModelALogPrior,
+        'model_B': priors.ModelBLogPrior,
+        }
+
+# Update protocol
+model.set_fixed_form_voltage_protocol(protocol, protocol_times)
+
+# Create Pints stuffs
+n_non_model_p = 4  # Number of non-model parameters
+inducing_times = times[::1000] # inducing or speudo points for the FITC GP
+inducing_voltage = voltage[::1000]
+problem = pints.SingleOutputProblem(model, times, data)
+loglikelihood = DiscrepancyLogLikelihood(problem, inducing_times,
+        voltage=voltage, inducing_voltage=inducing_voltage, downsample=None)
+logmodelprior = LogPrior[info_id](transform_to_model_param,
+        transform_from_model_param)
+# Priors for discrepancy
+# This will have considerable mass at the initial value
+lognoiseprior = HalfNormalLogPrior(sd=25, transform=True)
+logrhoprior_t = InverseGammaLogPrior(alpha=5, beta=5, transform=True)
+logrhoprior_v = InverseGammaLogPrior(alpha=5, beta=5, transform=True)
+logrhoprior = pints.ComposedLogPrior(logrhoprior_t, logrhoprior_v)
+logkersdprior = InverseGammaLogPrior(alpha=5, beta=5, transform=True)
+# Compose all priors
+logprior = pints.ComposedLogPrior(logmodelprior, lognoiseprior, logrhoprior,
+        logkersdprior)
+logposterior = pints.LogPosterior(loglikelihood, logprior)
+
+# Check logposterior is working fine
+priorparams = np.copy(info.base_param)
+transform_priorparams = transform_from_model_param(priorparams)
+# Stack non-model parameters together
+initial_rho = [0.5, 0.5]  # Kernel hyperparameter \rho
+initial_ker_sigma = 5.0  # Kernel hyperparameter \ker_sigma
+priorparams = np.hstack((priorparams, noise_sigma, initial_rho,
+        initial_ker_sigma))
+transform_priorparams = np.hstack((transform_priorparams, np.log(noise_sigma),
+        np.log(initial_rho), np.log(initial_ker_sigma)))
+print('Posterior at prior parameters: ',
+        logposterior(transform_priorparams))
+for _ in range(10):
+    assert(logposterior(transform_priorparams) ==\
+            logposterior(transform_priorparams))
+
+# Load fitting results
+calloaddir = './out/' + info_id + '-gp-tv'
+load_seed = '542811797'
+fit_idx = [1, 2, 3]
+transform_x0_list = []
+
+print('MCMC starting point: ')
+for i in fit_idx:
+    f = '%s/%s-solution-%s-%s.txt' % (calloaddir, 'sinewave', load_seed, i)
+    p = np.loadtxt(f)
+    transform_x0_list.append(np.hstack((
+                    transform_from_model_param(p[:-n_non_model_p]),
+                    np.log(p[-n_non_model_p:]))))
+    print(transform_x0_list[-1])
+    print('Posterior: ', logposterior(transform_x0_list[-1]))
+
+# Run
+mcmc = pints.MCMCController(logposterior, len(transform_x0_list),
+        transform_x0_list, method=pints.AdaptiveCovarianceMCMC)
+n_iter = 100000
+mcmc.set_max_iterations(n_iter)
+mcmc.set_initial_phase_iterations(200)  # max 200 iterations for random walk
+mcmc.set_parallel(True)
+mcmc.set_chain_filename('%s/%s-chain.csv' % (savedir, saveas))
+mcmc.set_log_pdf_filename('%s/%s-pdf.csv' % (savedir, saveas))
+chains = mcmc.run()
+
+# De-transform parameters
+chains_param = np.zeros(chains.shape)
+for i, c in enumerate(chains):
+    c_tmp = np.copy(c)
+    # First the model ones
+    chains_param[i, :, :-n_non_model_p] = transform_to_model_param(
+            c_tmp[:, :-n_non_model_p])
+    # Then the discrepancy ones
+    chains_param[i, :, -n_non_model_p:] = np.exp((c_tmp[:, -n_non_model_p:]))
+    del(c_tmp)
+
+# Save (de-transformed version)
+pints.io.save_samples('%s/%s-chain.csv' % (savedir, saveas), *chains_param)
+
+# Plot
+# burn in and thinning
+chains_final = chains[:, int(0.5 * n_iter)::5, :]
+chains_param = chains_param[:, int(0.5 * n_iter)::5, :]
+
+transform_x0 = transform_x0_list[0]
+x0 = np.append(transform_to_model_param(transform_x0[:-n_non_model_p]),
+        np.exp(transform_x0[-n_non_model_p:]))
+
+pints.plot.pairwise(chains_param[0], kde=False, ref_parameters=x0)
+plt.savefig('%s/%s-fig1.png' % (savedir, saveas))
+plt.close('all')
+
+pints.plot.trace(chains_param, ref_parameters=x0)
+plt.savefig('%s/%s-fig2.png' % (savedir, saveas))
+plt.close('all')
+
--- ion-channel-models/method/sparse_gp_custom_likelihood.py
+++ ion-channel-models/method/sparse_gp_custom_likelihood.py
@@ -110,6 +110,63 @@ def _create_theano_likelihood_graph(data, t, ind_t, n_time, n_inducing_time,
             on_unused_input='ignore')
 
 
+def _create_theano_likelihood_graph_voltage(data, X, ind_X, n_X, n_inducing_X,
+        approx='FITC'):
+    """ 
+    Here we use theano to compile a comutational graph defining our discrepancy
+    likelihood. Note it just compiles this graph as a C program which will
+    get successively called in PINTS. Thus all the variables defined here are
+    simply placeholders.
+    """
+
+    rho = tt.dvector('rho')
+    ker_sigma = tt.dscalar('ker_sigma')
+    sigma = tt.dscalar('sigma')
+    time_V = theano.tensor.as_tensor_variable(X)
+    inducing_time_V = theano.tensor.as_tensor_variable(ind_X)
+    y = theano.tensor.as_tensor_variable(data)
+
+    current = tt.dvector('current')
+
+    cov_func = RbfKernel(rho, ker_sigma)
+
+    sigma2 = tt.square(sigma)
+    Kuu = cov_func(inducing_time_V)
+    Kuf = cov_func(inducing_time_V, time_V)
+ 
+    Luu = cholesky(stabilize(Kuu))
+    A = solve_lower(Luu, Kuf)
+    Qffd = tt.sum(A * A, 0)
+    
+    if approx == 'FITC':
+        Kffd = cov_func(time_V, diag=True)
+        Lamd = tt.clip(Kffd - Qffd, 0.0, np.inf) + sigma2
+        trace = 0.0
+    elif approx == 'VFE':
+        Lamd = tt.ones_like(Qffd) * sigma2
+        trace = ((1.0 / (2.0 * sigma2)) *
+                    (tt.sum(cov_func(time_V, diag=True)) -
+                    tt.sum(tt.sum(A * A, 0))))
+    else:  # DTC
+        Lamd = tt.ones_like(Qffd) * sigma2
+        trace = 0.0
+    
+    A_l = A / Lamd
+    L_B = cholesky(tt.eye(n_inducing_X) + tt.dot(A_l, tt.transpose(A)))
+    r = y - current
+    
+    r_l = r / Lamd
+    
+    c = solve_lower(L_B, tt.dot(A, r_l))
+    
+    constant = 0.5 * n_X * tt.log(2.0 * np.pi)
+    logdet = 0.5 * tt.sum(tt.log(Lamd)) + tt.sum(tt.log(tt.diag(L_B)))
+    quadratic = 0.5 * (tt.dot(r, r_l) - tt.dot(c, c))
+    ll = -1.0 * (constant + logdet + quadratic + trace)  
+    return theano.function([current, rho, ker_sigma, sigma], ll,
+            on_unused_input='ignore')
+
+
 def _create_theano_conditional_graph(data, t, ind_t, t_new, approx='FITC'):
     rho = tt.dscalar('rho')
     ker_sigma = tt.dscalar('ker_sigma')
@@ -156,36 +213,98 @@ def _create_theano_conditional_graph(data, t, ind_t, t_new, approx='FITC'):
                             on_unused_input='ignore')]
 
 
+def _create_theano_conditional_graph_voltage(data, X, ind_X, X_new,
+        approx='FITC'):
+    rho = tt.dvector('rho')
+    ker_sigma = tt.dscalar('ker_sigma')
+    sigma = tt.dscalar('sigma')
+    time_V = theano.tensor.as_tensor_variable(X)
+    time_new_V = theano.tensor.as_tensor_variable(X_new)
+    inducing_time_V = theano.tensor.as_tensor_variable(ind_X)
+    y = theano.tensor.as_tensor_variable(data)
+    current = tt.dvector('current')
+    current_new = tt.dvector('current_new')
+
+    cov_func = RbfKernel(rho, ker_sigma)
+
+    sigma2 = tt.square(sigma)
+    Kuu = cov_func(inducing_time_V)
+    Kuf = cov_func(inducing_time_V, time_V)
+    Luu = cholesky(stabilize(Kuu))
+    A = solve_lower(Luu, Kuf)
+    Qffd = tt.sum(A * A, 0)
+    if approx == "FITC":
+        Kffd = cov_func(time_V, diag=True)
+        Lamd = tt.clip(Kffd - Qffd, 0.0, np.inf) + sigma2
+    else:  # VFE or DTC
+        Lamd = tt.ones_like(Qffd) * sigma2
+    A_l = A / Lamd
+    L_B = cholesky(tt.eye(inducing_time_V.shape[0]) \
+            + tt.dot(A_l, tt.transpose(A)))
+    r = y - current
+    r_l = r / Lamd
+    c = solve_lower(L_B, tt.dot(A, r_l))
+    Kus = cov_func(inducing_time_V, time_new_V)
+    As = solve_lower(Luu, Kus)
+    mu = current_new \
+            + tt.dot(tt.transpose(As), solve_upper(tt.transpose(L_B), c))
+    C = solve_lower(L_B, As)
+
+    Kss = cov_func(time_new_V, diag=True)
+    var = Kss - tt.sum(tt.square(As), 0) + tt.sum(tt.square(C), 0)
+    var += sigma2
+    return [theano.function([current, current_new, rho, ker_sigma, sigma], mu,
+                             on_unused_input='ignore'),
+            theano.function([current, current_new, rho, ker_sigma, sigma], var,
+                             on_unused_input='ignore')]
+
+
 class DiscrepancyLogLikelihood(pints.ProblemLogLikelihood):
     """
     This class defines a custom loglikelihood which implements a
     discrepancy model where the discrepancy is modelled 
     as a reduced rank Gaussian process using the FITC likelihood
     """
-    def __init__(self, problem, inducing_times, downsample=None,
-            temperature=None):
+    def __init__(self, problem, inducing_times, voltage=None,
+            inducing_voltage=None, downsample=None, temperature=None):
         super(DiscrepancyLogLikelihood, self).__init__(problem)
 
         self._no = problem.n_outputs()
         self._np = problem.n_parameters()
         self._nt = problem.n_times() 
         self._inducing_times = inducing_times
+        self._inducing_voltage = inducing_voltage
         self._nu = len(inducing_times)
         self._downsample = downsample if downsample is not None else 1
 
-        self._nds = 3
         data = self._values[::self._downsample].reshape((-1,))
-        t = self._times[::self._downsample].reshape((-1,1)) 
-        ind_t = self._inducing_times.reshape((-1,1))
-        self._loglikelihood = _create_theano_likelihood_graph(data, t, ind_t,
-                self._nt, self._nu)
+        t = self._times[::self._downsample].reshape((-1, 1)) 
+        ind_t = self._inducing_times.reshape((-1, 1))
+        v = self._voltage[::self._downsample].reshape((-1, 1))
+        ind_v = self._inducing_voltage.reshape((-1, 1))
+
+        if self._voltage is not None:
+            self._nds = 4
+            x = np.concatenate((t, v), axis=1)
+            ind_x = np.concatenate((ind_t, ind_v), axis=1)
+            self._loglikelihood = _create_theano_likelihood_graph_voltage(data,
+                    x, ind_x, self._nt, self._nu)
+        else:
+            self._nds = 3
+            self._loglikelihood = _create_theano_likelihood_graph(data, t,
+                    ind_t, self._nt, self._nu)
+
         self._n_parameters = self._np + self._nds
-        
+
         self._temperature = temperature
 
     def __call__(self, x):
         # Split parameters
-        _sigma, _rho, _ker_sigma = x[-self._nds:]
+        if self._voltage is not None:
+            _sigma, _rho1, _rho2, _ker_sigma = x[-self._nds:]
+            _rho = np.append(_rho1, _rho2)
+        else:
+            _sigma, _rho, _ker_sigma = x[-self._nds:]
         model_params = x[:-self._nds]
 
         # De-transform parameters
