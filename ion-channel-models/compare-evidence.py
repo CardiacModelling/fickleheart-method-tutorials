7ac19a76bc2c569ed9a4cf31efa578283df0c336
Chon Lok Lei <chonloklei@gmail.com>
2019-12-11 22:01:25 +0000

    Udpate evidence table size

--- ion-channel-models/compare-evidence.py
+++ ion-channel-models/compare-evidence.py
@@ -13,10 +13,10 @@ def rmse(t1, t2):
 error_measure = rmse
 
 model_list = ['A', 'B']
-predict_list = ['sinewave', 'staircase', 'ap'][::-1]
+predict_list = ['Sinewave', 'Staircase', 'AP'][::-1]
 discrepancy_list = ['', '-gp', '-gp-ov', '-arma_2_2']
 load_list = ['-iid', '-gp', '-gp', '-armax']
-discrepancy_names = ['iid noise', 'GP(t)', 'GP(O, V)', 'ARMAX(2, 2)']
+discrepancy_names = ['iid noise', 'GP(t)', 'GP(O, V)', 'ARMA(2, 2)']
 
 try:
     which_model = sys.argv[1] 
@@ -75,7 +75,7 @@ def row(axes, y, data, std=None):
             text += ' (' + fmat.format(std[i]).strip() + ')'
         plt.text(x + w / 2., y + h / 2., text, **targs)
 
-plt.figure(figsize=(7, 1.8))
+plt.figure(figsize=(5.2, 1.8))
 plt.subplots_adjust(0.005, 0.005, 0.995, 0.995)
 
 xperbox = 4.
@@ -92,16 +92,16 @@ for i, n in enumerate(names):
 #plt.text(xperbox * 4, 4.5, 'Fitted with GP(O, V)', **targs)
 #plt.plot([xperbox * 3 + 0.25, xperbox * 3 + xperbox * 2 - 0.25], [4.2]*2,
 #        c='#dddddd')
-#plt.text(xperbox * 6, 4.5, 'Fitted with ARMAX(2, 2)', **targs)
+#plt.text(xperbox * 6, 4.5, 'Fitted with ARMA(2, 2)', **targs)
 #plt.plot([xperbox * 5 + 0.25, xperbox * 5 + xperbox * 2 - 0.25], [4.2]*2,
 #        c='#dddddd')
 
 label = 'Model %s' % which_model
-plt.text(-1.75, 3.5, label, {'weight': 'normal', 'size': 14}, **targs)
+plt.text(-2, 3.5, label, {'weight': 'normal', 'size': 14}, **targs)
 
 for i, n in enumerate(predict_list):
-    plt.text(-1.5, i + .5, n, **targs)
-plt.text(-4.5, 2.5, 'Calibration', **targs)
+    plt.text(-1.5, i + .5, n, **targs, fontsize=9)
+plt.text(-4.5, 2.5, 'Calibration', **targs, fontsize=9)
 plt.plot([-2.9]*2, [2.1, 2.9], c='#dddddd')
 plt.text(-4.5, 1.0, 'Prediction', **targs)
 plt.plot([-2.9]*2, [0.2, 1.8], c='#dddddd')
