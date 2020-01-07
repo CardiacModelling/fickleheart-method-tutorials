3c4bce5e3cb7e6ba7f091857d086a13ef64877e8
Chon Lok Lei <chonloklei@gmail.com>
2020-01-07 17:54:02 +0000

    Slight fixes

--- ion-channel-models/posterior-all.sh
+++ ion-channel-models/posterior-all.sh
@@ -12,21 +12,21 @@ python posterior.py A sinewave
 python posterior.py A staircase
 python posterior.py A ap
 
-python posterior-arma.py A sinewave
-python posterior-arma.py A staircase
-python posterior-arma.py A ap
+python posterior-arma.py A 2 2 sinewave
+python posterior-arma.py A 2 2 staircase
+python posterior-arma.py A 2 2 ap
 
 python posterior-gp.py A sinewave
 python posterior-gp.py A staircase
 python posterior-gp.py A ap
 
-python posterior-gp-tv.py A sinewave
-python posterior-gp-tv.py A staircase
-python posterior-gp-tv.py A ap
+#python posterior-gp-tv.py A sinewave
+#python posterior-gp-tv.py A staircase
+#python posterior-gp-tv.py A ap
 
-python posterior-gp-ov.py A sinewave -v
-python posterior-gp-ov.py A staircase -v
-python posterior-gp-ov.py A ap -v
+#python posterior-gp-ov.py A sinewave -v
+#python posterior-gp-ov.py A staircase -v
+#python posterior-gp-ov.py A ap -v
 
 python posterior-gp-ov.py A sinewave -ov
 python posterior-gp-ov.py A staircase -ov
@@ -45,13 +45,13 @@ python posterior-gp.py B sinewave
 python posterior-gp.py B staircase
 python posterior-gp.py B ap
 
-python posterior-gp-tv.py B sinewave
-python posterior-gp-tv.py B staircase
-python posterior-gp-tv.py B ap
+#python posterior-gp-tv.py B sinewave
+#python posterior-gp-tv.py B staircase
+#python posterior-gp-tv.py B ap
 
-python posterior-gp-ov.py B sinewave -v
-python posterior-gp-ov.py B staircase -v
-python posterior-gp-ov.py B ap -v
+#python posterior-gp-ov.py B sinewave -v
+#python posterior-gp-ov.py B staircase -v
+#python posterior-gp-ov.py B ap -v
 
 python posterior-gp-ov.py B sinewave -ov
 python posterior-gp-ov.py B staircase -ov
